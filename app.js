588312db3688296af6a083b03a37f47abcef7d5d
Luke Maupin <lukecmaupin@gmail.com>
2021-10-28 13:44:58 -0400

    Amends base ansers and adds bonus answers

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const portillos = document.getElementById(`portillos`);
 
 // 2
-const images = document.getElementByTagName(`img`);
+const images = document.getElementsByTagName(`img`);
 
 // 3
-const centered = document.getElementByClassName(`center`);
+const centered = document.getElementsByClassName(`center`);
 
 // 4
-const ginosEast = document.querySelector(`d.ginos`);
+const ginosEast = document.querySelector(`#ginos`);
 
 // 5
 const pTags = document.querySelectorAll(`p`);
@@ -20,7 +20,7 @@ const h1 = document.querySelector(`h1`);
 h1.innerText = `Matt's Favorite Places To Eat In Sweet Home Chicago`;
 
 // 7a
-const others = document.querySelector(`others`);
+const others = document.querySelector(`#others`);
 
 // 7b
 others.innerHTML = `<h3>Other favorites</h3>`;
@@ -32,10 +32,10 @@ const aTag = document.querySelector(`a`);
 aTag.href = `https://www.choosechicago.com/articles/food-drink/5-classic-chicago-foods/`;
 
 // 9
-h1.classList.add = `background` `text-color`;
+h1.classList.add(`background`, `text-color`);
 
 // 10
-h1.classList.remove = `background`;
+h1.classList.remove(`background`);
 
 // 11a
 const h4 = document.createElement(`h4`);
@@ -53,7 +53,36 @@ const h5 = document.createElement(`h5`);
 h5.innerText = `See you in the Windy City sometime!`;
 
 // 12c
-document.a.insertAdjacentElement(`h5`);
+aTag.insertAdjacentElement(`afterend`, h5);
 
 // 13
-document.remove(`ul.li`);
+document.querySelector(`ul>li`).remove();
+
+// BONUS
+
+// 14a
+const divs = document.getElementsByTagName(`div`);
+
+// 14b
+for (d of divs){
+    d.classList.toggle(`background`);
+}
+
+// 15a
+const hungry = [
+    `Thanks`,
+    `A`,
+    `Lot`,
+    `Now`,
+    `I`,
+    `Am`,
+    `Hungry`
+];
+
+// 15b
+const end = document.querySelector(`body`);
+for (a of hungry){
+    const span = document.createElement(`span`);
+    span.innerHTML = `${a}`;
+    end.append(span);
+}
